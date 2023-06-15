e3d5e5fb1e614680878296bdfe57acb70d8c06ee
officename <ankitrajput2730@gmail.com>
2023-06-15 12:26:34 +0530

    huyuyguy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Netfilx from './Component/Netfilx'
 import Image from './Component/Image'
 import "./App.css";
+import Miniproject from './Component/Miniproject';
 const App = () => {
   return (
-    <div>
+    <div >
       {/* < Netfilx/> */}
-      <Image/>
+      {/* <Image/> */}
+      <Miniproject/>
     </div>
   )
 }
